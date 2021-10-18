ac1526eed9754f9e6c497fd6fbfbf442a8a8fa10
min <minwang.bus@gmail.com>
2021-10-18 16:50:55 -0400

    toppological Sorting

--- bfs.py
+++ bfs.py
@@ -134,3 +134,142 @@ def copy_neighbors(self, nodes, mapping):
         for neighbor in node.neighbors:
             newNeighbor = mapping[neighbor]
             curNode.neighbors.append(newNeighbor)
+
+
+# 611 · Knight Shortest Path
+# Description
+# Given a knight in a chessboard (a binary matrix with 0 as empty and 1 as barrier) with a source position, find the shortest 
+# path to a destination position, return the length of the route.
+# Return -1 if destination cannot be reached.
+# source and destination must be empty.
+# Knight can not enter the barrier.
+# Path length refers to the number of steps the knight takes.
+# If the knight is at (x, y), he can get to the following positions in one step:
+
+# (x + 1, y + 2)
+# (x + 1, y - 2)
+# (x - 1, y + 2)
+# (x - 1, y - 2)
+# (x + 2, y + 1)
+# (x + 2, y - 1)
+# (x - 2, y + 1)
+# (x - 2, y - 1)
+
+# Example 1:
+# Input:
+# [[0,0,0],
+#  [0,0,0],
+#  [0,0,0]]
+# source = [2, 0] destination = [2, 2] 
+# Output: 2
+# Explanation:
+# [2,0]->[0,1]->[2,2]
+
+# Example 2:
+# Input:
+# [[0,1,0],
+#  [0,0,1],
+#  [0,0,0]]
+# source = [2, 0] destination = [2, 2] 
+# Output:-1
+
+"""
+Definition for a point.
+class Point:
+    def __init__(self, a=0, b=0):
+        self.x = a
+        self.y = b
+"""
+DIRECTIONS = [(1, 2),
+(1, - 2),
+(- 1, 2),
+(- 1, -2),
+(2, 1),
+(2, - 1),
+(- 2, 1),
+(- 2, - 1)]
+
+class Solution:
+    """
+    @param grid: a chessboard included 0 (false) and 1 (true)
+    @param source: a point
+    @param destination: a point
+    @return: the shortest path 
+    """
+    def shortestPath(self, grid, source, destination):
+        # write your code here
+
+        queue = collections.deque([(source.x, source.y)])
+        visited = {(source.x, source.y): 0}
+        
+        while queue:
+            curX, curY = queue.popleft()
+            if (curX, curY) == (destination.x, destination.y):
+                return visited[(curX,curY)]
+            for deltaX, deltaY in DIRECTIONS:
+                newX, newY = curX + deltaX, curY + deltaY
+                
+                if not self.is_valid(grid, newX, newY, visited):
+                    continue 
+                queue.append((newX, newY))
+                visited[(newX, newY)] = visited[(curX, curY)] + 1
+        return -1 
+        
+    def is_valid(self, grid, x, y, visited):
+        n, m = len(grid), len(grid[0])
+        if not (0 <= x < n and 0 <= y < m):
+            return False 
+        if (x, y) in visited:
+            return False
+        return grid[x][y] != 1
+
+# 127 · Topological Sorting
+# Description
+# Given an directed graph, a topological order of the graph nodes is defined as follow:
+# For each directed edge A -> B in graph, A must before B in the order list.
+# The first node in the order can be any node in the graph with no nodes direct to it.
+# Find any topological order for the given graph.
+# You can assume that there is at least one topological order in the graph.
+# Learn more about representation of graphs
+# The number of graph nodes <= 5000
+
+# Example 1:
+# Input:
+# graph = {0,1,2,3#1,4#2,4,5#3,4,5#4#5}
+# Output:
+# [0, 1, 2, 3, 4, 5]
+"""
+class DirectedGraphNode:
+     def __init__(self, x):
+         self.label = x
+         self.neighbors = []
+"""
+
+class Solution:
+    """
+    @param graph: A list of Directed graph node
+    @return: Any topological order for the given graph.
+    """
+    def topSort(self, graph):
+        # write your code here
+        node_to_indegree = self.node_degree_hash(graph)
+        queue = collections.deque([headNode for headNode in node_to_indegree if node_to_indegree[headNode] == 0])
+        order = []
+        while queue:
+            curNode = queue.popleft()
+            order.append(curNode)
+            for neighbor in curNode.neighbors:
+                node_to_indegree[neighbor] -= 1
+                if node_to_indegree[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        return order
+
+    def node_degree_hash(self, graph):
+        node_to_indegree = {node: 0 for node in graph}
+        for node in node_to_indegree:
+            for neighbor in node.neighbors:
+                node_to_indegree[neighbor] += 1
+
+        return node_to_indegree
+
