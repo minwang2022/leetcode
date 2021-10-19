978860d5a8cc3a4381173c12f7895715d2d3a58f
min <minwang.bus@gmail.com>
2021-10-19 01:00:07 -0400

    knight II questions

--- bfs.py
+++ bfs.py
@@ -428,3 +428,66 @@ class Solution:
             return order
         return []
 
+# 630 · Knight Shortest Path II
+# Description
+# Given a knight in a chessboard n * m (a binary matrix with 0 as empty and 1 as barrier). 
+# the knight initialze position is (0, 0) and he wants to reach position (n - 1, m - 1), 
+# Knight can only be from left to right. Find the shortest path to the destination position, 
+# return the length of the route. Return -1 if knight can not reached.
+# If the knight is at (x, y), he can get to the following positions in one step:
+# (x + 1, y + 2)
+# (x - 1, y + 2)
+# (x + 2, y + 1)
+# (x - 2, y + 1)
+
+# Example 1:
+# Input:
+# [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+# Output:
+# 3
+# Explanation:
+# [0,0]->[2,1]->[0,2]->[2,3]
+
+# Example 2:
+# Input:
+# [[0,1,0],[0,0,1],[0,0,0]]
+# Output:
+# -1
+DIRECTIONS = [(1, 2),
+(- 1, 2),
+(2, 1),
+(-2, 1)]
+
+class Solution:
+    """
+    @param grid: a chessboard included 0 and 1
+    @return: the shortest path
+    """
+    def shortestPath2(self, grid):
+        # write your code here
+        queue = collections.deque([(0,0)])
+        steps = {(0, 0): 0}
+        n, m = len(grid), len(grid[0])
+        while queue:
+            cur_x, cur_y = queue.popleft()
+            if (cur_x, cur_y) == (n - 1, m - 1):
+                return steps[(cur_x, cur_y)]
+            for d_x, d_y in DIRECTIONS:
+                new_x, new_y = cur_x + d_x, cur_y + d_y
+
+                if not self.is_valid(grid, new_x, new_y, n, m):
+                    continue
+                if (new_x, new_y) in steps:
+                    continue 
+
+                queue.append((new_x, new_y))
+                steps[(new_x, new_y)] = steps[(cur_x, cur_y)] + 1
+
+        return -1
+                    
+    def is_valid(self, grid, x, y, n, m):
+        if not (0 <= x < n and 0 <= y < m):
+            return False 
+        if grid[x][y] == 1:
+            return False 
+        return not grid[x][y]
