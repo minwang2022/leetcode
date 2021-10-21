2bca4b1542981cb7cb5d2742211cc67bb1c31672
min <minwang.bus@gmail.com>
2021-10-20 22:55:49 -0400

    valid tree

--- bfs.py
+++ bfs.py
@@ -727,3 +727,50 @@ class Solution:
             return False
 
         return grid[x][y] == 0
+
+# 178 · Graph Valid Tree
+# Description
+# Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
+# You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+
+# Example 1:
+# Input: n = 5 edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
+# Output: true
+
+# Example 2:
+# Input: n = 5 edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]
+# Output: false.
+
+class Solution:
+    """
+    @param n: An integer
+    @param edges: a list of undirected edges
+    @return: true if it's a valid tree, or false
+    """
+    def validTree(self, n, edges):
+        # write your code here
+        numOfEdge = len(edges)
+        if numOfEdge != n - 1:
+            return False
+        adjacent = [[0] * n for _ in range(n)] 
+        for i in range(numOfEdge):
+            u = edges[i][0]
+            v = edges[i][1]
+            adjacent[u][v] = adjacent[v][u] = 1
+        visit = [0] * n
+        visit[0] = 1
+        root, numOfVisited = 0, 1
+        q = collections.deque()
+        q.append(root)
+        while len(q) != 0:
+            root = q.popleft()
+            for i in range(n):
+                if adjacent[root][i] != 1:
+                    continue
+                if visit[i] == 0:
+                    visit[i] = 1
+                    numOfVisited += 1
+                    q.append(i)
+        if numOfVisited == n:
+            return True
+        return False
