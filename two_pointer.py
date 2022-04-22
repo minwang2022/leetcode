1a7a8d4653fb0ed6abbbe506a5c9a530a266490b
min <minwang.bus@gmail.com>
2022-04-22 14:48:49 -0400

    two pointer4

--- two_pointer.py
+++ two_pointer.py
@@ -140,3 +140,54 @@ class Solution:
                 
         return res 
 
+
+# 15. 3Sum
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+# Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+# Example 1:
+
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+# Example 2:
+
+# Input: nums = []
+# Output: []
+# Example 3:
+
+# Input: nums = [0]
+# Output: []
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        l = len(nums)
+        res = []
+        if l < 3:
+            return res 
+        
+        for i in range(l):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            start, end = i + 1, len(nums) - 1
+            while start < end:
+                cur_sum = nums[start] + nums[end] + nums[i]
+                if cur_sum < 0:
+                    start += 1
+                elif cur_sum > 0:
+                    end -= 1
+                else:
+                    res.append((nums[i], nums[start], nums[end]))
+                    while start < end and nums[start] == nums[start + 1]:
+                        start += 1
+                    while start < end and nums[end] == nums[end -1]:
+                        end -= 1
+                    start += 1
+                    end -= 1
+            
+        return res
+            
