68d4ee4861642417a76b7ab0799e97da6472badc
min <minwang.bus@gmail.com>
2022-04-22 15:49:58 -0400

    two pointer4

--- two_pointer.py
+++ two_pointer.py
@@ -190,4 +190,54 @@ class Solution:
                     end -= 1
             
         return res
+
+# 18. 4Sum
+# Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+# 0 <= a, b, c, d < n
+# a, b, c, and d are distinct.
+# nums[a] + nums[b] + nums[c] + nums[d] == target
+# You may return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,0,-1,0,-2,2], target = 0
+# Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+# Example 2:
+
+# Input: nums = [2,2,2,2,2], target = 8
+# Output: [[2,2,2,2]]
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def findNsum(nums, target, N, cur):
+            if len(nums) < N or N < 2 or nums[0] * N > target or nums[-1] * N < target:  # if minimum possible sum (every element is first element) > target 
+                return  # or maximum possible sum (every element is first element) < target, it's impossible to get target anyway          
+            if N == 2:  # 2-sum problem
+                l, r = 0, len(nums) - 1
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        res.append(cur + [nums[l], nums[r]])
+                        while l < r and nums[l] == nums[l - 1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r - 1]:
+                            r -= 1
+                        l += 1
+                        r -= 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else:  # reduce to N-1 sum problem
+                for i in range(len(nums) - N + 1):
+                    if i == 0 or nums[i - 1] != nums[i]:
+                        findNsum(nums[i + 1 :], target - nums[i], N - 1, cur + [nums[i]])
+
+        res = []
+        findNsum(sorted(nums), target, 4, [])
+        return res
+            
             
