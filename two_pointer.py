da4759ec14355e08ffd9a4d2a5012cc2e000d5a1
min <minwang.bus@gmail.com>
2022-04-25 14:58:03 -0400

    two pointer5

--- two_pointer.py
+++ two_pointer.py
@@ -241,3 +241,35 @@ class Solution:
         return res
             
             
+# 49. Group Anagrams
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+# Example 1:
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+# Example 2:
+
+# Input: strs = [""]
+# Output: [[""]]
+# Example 3:
+
+# Input: strs = ["a"]
+# Output: [["a"]]
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+       
+        visited = {}
+        for word in strs:
+            new_word = ''.join(sorted(word))
+            if new_word in visited:
+                visited[new_word].append(word)
+            else:
+                visited[new_word] = [word]
+        
+        return visited.values()
+                
