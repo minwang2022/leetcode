28625dd5060a41c1e542d76b8a5ed076b3fde3a3
min <minwang.bus@gmail.com>
2022-04-25 23:26:05 -0400

    two pointer9

--- two_pointer.py
+++ two_pointer.py
@@ -379,4 +379,59 @@ class Solution:
             else:
                 nums1[m + n -1] = nums2[n -1]
                 n -= 1
-                
+            
+# 143 · Sort Colors II
+# Description
+# Given an array of n objects with k different colors (numbered from 1 to k), sort them so that objects of the same color are adjacent, with the colors in the order 1, 2, ... k.
+
+# You are not suppose to use the library's sort function for this problem.
+# k <= n
+# Example
+# Example1
+
+# Input: 
+# [3,2,2,1,4] 
+# 4
+# Output: 
+# [1,2,2,3,4]
+# Example2
+
+# Input: 
+# [2,1,1,2,2] 
+# 2
+# Output: 
+# [1,1,2,2,2]
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param colors: A list of integer
+    @param k: An integer
+    @return: nothing
+    """
+    def sort_colors2(self, colors: List[int], k: int):
+        # write your code here
+        self.sort(colors, 1, k, 0, len(colors) - 1)
+        
+    def sort(self, colors, color_from, color_to, index_from, index_to):
+        if color_from == color_to or index_from == index_to:
+            return
+            
+        color = (color_from + color_to) // 2
+        
+        left, right = index_from, index_to
+        while left <= right:
+            while left <= right and colors[left] <= color:
+                left += 1
+            while left <= right and colors[right] > color:
+                right -= 1
+            if left <= right:
+                colors[left], colors[right] = colors[right], colors[left]
+                left += 1
+                right -= 1
+        
+        self.sort(colors, color_from, color, index_from, right)
+        self.sort(colors, color + 1, color_to, left, index_to)
