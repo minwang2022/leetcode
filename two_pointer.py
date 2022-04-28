f6ee084e57b86d343da69b1a621ef65f846768ea
min <minwang.bus@gmail.com>
2022-04-27 21:23:56 -0400

    two pointer13

--- two_pointer.py
+++ two_pointer.py
@@ -565,3 +565,33 @@ class Solution:
                 currhi, currlo = max(currhi, n), min(currlo, n)
             ans = max(ans, currlo - prevhi)
         return ans
+
+
+# 169. Majority Element
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+# Example 1:
+
+# Input: nums = [3,2,3]
+# Output: 3
+# Example 2:
+
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        target = None 
+        count = 0 
+        
+        for num in nums:
+            if target == num:
+                count += 1
+            else:
+                count -= 1
+            
+            if count <= 0:
+                target = num
+                count = 1
+        return target
