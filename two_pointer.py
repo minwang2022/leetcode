e48417e995937c9d22e95b93c98506144230c142
min <minwang.bus@gmail.com>
2022-05-01 16:21:06 -0400

    two pointer21

--- two_pointer.py
+++ two_pointer.py
@@ -910,3 +910,36 @@ class Solution:
             return self.quickSelect(nums, left, end, k)
 
         return nums[k]
+
+# 217. Contains Duplicate
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+#O(nlogn)  => built-in sorting takes nlogn time complexity 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        nums.sort()
+        
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True 
+        
+        return False
+        
+#O(n) => set creation takes o(len(s)) => o(n) time complexity
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+      
+        return len(nums) != len(set(nums))
