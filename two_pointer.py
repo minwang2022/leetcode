0d52000c1cc2bcf22ccd51139eff18a0e7afbd01
min <minwang.bus@gmail.com>
2022-05-01 14:57:57 -0400

    two pointer20

--- two_pointer.py
+++ two_pointer.py
@@ -863,7 +863,7 @@ class Solution:
         mid = (start + end ) // 2 
         left = self.mergeSort(nums, start, mid)
         right = self.mergeSort(nums, mid + 1,end)
-        print(left,"right", right)
+        
         return self.merge(left, right)
         
     def merge(self, left, right):
@@ -878,4 +878,35 @@ class Solution:
         res.extend(left[l:] or right[r:])
         return res
 
+#quick-select O(n) time, O(1) space
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
         
+     
+        return self.quickSelect(nums, 0, len(nums) - 1, len(nums) -k)
+        
+    def quickSelect(self, nums, start, end, k):
+
+        left, right = start, end
+        print(left, right)
+        pivot = nums[(start + end) //2] 
+        print("pivot", pivot)
+        print("nums", nums)
+        while left <= right:
+            while left <= right and nums[left] < pivot:
+                left += 1
+                
+            while left <= right and nums[right] > pivot:
+                right -= 1
+            
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        if k <= right:
+            return self.quickSelect(nums, start, right, k)
+        if k >= left:
+            return self.quickSelect(nums, left, end, k)
+
+        return nums[k]
