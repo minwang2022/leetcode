36ecca7c34316cceb930e7e4387dcac83d06bab5
min <minwang.bus@gmail.com>
2022-05-02 23:44:49 -0400

    two_pointer 24 heap

--- two_pointer.py
+++ two_pointer.py
@@ -1059,3 +1059,56 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
     
         return sum(range(len(nums)+1)) - sum(nums)
+
+# 295. Find Median from Data Stream
+# The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
+
+# For example, for arr = [2,3,4], the median is 3.
+# For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+# Implement the MedianFinder class:
+
+# MedianFinder() initializes the MedianFinder object.
+# void addNum(int num) adds the integer num from the data stream to the data structure.
+# double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+# Example 1:
+
+# Input
+# ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+# [[], [1], [2], [], [3], []]
+# Output
+# [null, null, null, 1.5, null, 2.0]
+
+# Explanation
+# MedianFinder medianFinder = new MedianFinder();
+# medianFinder.addNum(1);    // arr = [1]
+# medianFinder.addNum(2);    // arr = [1, 2]
+# medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+# medianFinder.addNum(3);    // arr[1, 2, 3]
+# medianFinder.findMedian(); // return 2.0
+
+class MedianFinder:
+
+    def __init__(self):
+        import heapq
+        self.small = [] # maxheap
+        self.large = [] # minheap
+        
+    def addNum(self, num: int) -> None:
+        if len(self.small) == len(self.large):
+            heapq.heappush(self.small,-num)
+            
+            tmp = heapq.heappop(self.small)
+            heapq.heappush(self.large,-tmp)
+            
+        else:
+            heapq.heappush(self.large,num)
+            tmp = heapq.heappop(self.large)
+            heapq.heappush(self.small,-tmp)
+
+    def findMedian(self) -> float:
+        if len(self.small) == len(self.large):
+            return (-self.small[0] + self.large[0])/2
+        else:
+            return self.large[0]
+            
