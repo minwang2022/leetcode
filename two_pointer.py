2542728fd3add16cec53c64f8084ae0d82009fc3
min <minwang.bus@gmail.com>
2022-05-02 22:15:12 -0400

    two_pointer 21.1

--- two_pointer.py
+++ two_pointer.py
@@ -978,3 +978,32 @@ class Solution:
         
         return res 
             
+import collections
+
+#O(n) time, O(k) space
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]: 
+        ctr = collections.Counter()
+        for n in nums:
+
+            ctr[n] += 1
+            if len(ctr) == 3:
+                
+                ctr -= collections.Counter(set(ctr))
+               
+        return [n for n in ctr if nums.count(n) > len(nums)/3]
+
+#O(n) time, O(1) space
+import collections
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]: 
+        ctr = collections.Counter()
+        for n in nums:
+
+            ctr[n] += 1
+            if len(ctr) == 3:
+                
+                ctr -= collections.Counter(set(ctr))
+               
+        return [n for n in ctr if nums.count(n) > len(nums)/3]
+        
