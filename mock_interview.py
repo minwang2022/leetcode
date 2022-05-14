55121b4f5b5f4745035a0d4694d3510cfa8ee3ac
min <minwang.bus@gmail.com>
2022-05-14 19:34:47 -0400

    mock interview tree

--- mock_interview.py
+++ mock_interview.py
@@ -188,3 +188,58 @@ class Solution:
         
     #     travel(head)
     #     return head
+
+# 117. Populating Next Right Pointers in Each Node II
+# Given a binary tree
+
+# struct Node {
+#   int val;
+#   Node *left;
+#   Node *right;
+#   Node *next;
+# }
+# Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+# Initially, all next pointers are set to NULL.
+
+# Example 1:
+
+# Input: root = [1,2,3,4,5,null,7]
+# Output: [1,#,2,3,#,4,5,7,#]
+# Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+# Example 2:
+
+# Input: root = []
+# Output: []
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+from collections import deque
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        que = deque([root])
+        while que:
+            n = len(que)
+            for i in range(len(que)):
+                cur = que.popleft()
+                if cur.left:
+                    que.append(cur.left)
+                if cur.right:
+                    que.append(cur.right)
+                
+                if i == n - 1:
+                    cur.next = None 
+                    continue 
+                
+                cur.next = que[0]
+        return root
