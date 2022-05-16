0a647f5cbb77ba872ea0b964cde415dcf24ae457
min <minwang.bus@gmail.com>
2022-05-15 21:47:53 -0400

    mock invalid transactions

--- mock_interview.py
+++ mock_interview.py
@@ -476,3 +476,75 @@ class Solution:
             visited[s[i]] = i
         
         return longest
+
+# 1169. Invalid Transactions
+# A transaction is possibly invalid if:
+
+# the amount exceeds $1000, or;
+# if it occurs within (and including) 60 minutes of another transaction with the same name in a different city.
+# You are given an array of strings transaction where transactions[i] consists of comma-separated values representing the name, time (in minutes), amount, and city of the transaction.
+
+# Return a list of transactions that are possibly invalid. You may return the answer in any order.
+
+# Example 1:
+
+# Input: transactions = ["alice,20,800,mtv","alice,50,100,beijing"]
+# Output: ["alice,20,800,mtv","alice,50,100,beijing"]
+# Explanation: The first transaction is invalid because the second transaction occurs within a difference of 60 minutes, have the same name and is in a different city. Similarly the second one is invalid too.
+# Example 2:
+
+# Input: transactions = ["alice,20,800,mtv","alice,50,1200,mtv"]
+# Output: ["alice,50,1200,mtv"]
+# Example 3:
+
+# Input: transactions = ["alice,20,800,mtv","bob,50,1200,mtv"]
+# Output: ["bob,50,1200,mtv"]
+
+import collections
+
+class Solution:
+    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        
+        r = {}
+                
+        inv = []        
+        for i in transactions:
+            split = i.split(",")
+            name = str(split[0])
+            time = int(split[1])
+            amount = int(split[2])
+            city = str(split[3])
+            
+            if time not in r:
+                r[time] = {
+                    name: [city]
+                }
+            else:
+                if name not in r[time]:
+                    r[time][name]=[city]
+                else:
+                    r[time][name].append(city)
+                    
+        
+        for i in transactions:
+            split = i.split(",")
+            name = str(split[0])
+            time = int(split[1])
+            amount = int(split[2])
+            city = str(split[3])
+            
+            
+            if amount > 1000:
+                inv.append(i)
+                continue
+            
+            for j in range(time-60, time+61):
+                if j not in r:
+                    continue
+                if name not in r[j]:
+                    continue
+                if len(r[j][name]) > 1 or (r[j][name][0] != city):
+                    inv.append(i)
+                    break
+                                        
+        return inv   
