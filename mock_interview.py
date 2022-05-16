4082c70517cbd59548f1951bb10f3ea71dc07153
min <minwang.bus@gmail.com>
2022-05-15 20:31:07 -0400

    mock longest substring without repeating characters

--- mock_interview.py
+++ mock_interview.py
@@ -440,3 +440,39 @@ class Solution:
 #         for k in range(n + 1):
 #             backtrack()
 #         return output
+
+
+
+# 3. Longest Substring Without Repeating Characters
+# Given a string s, find the length of the longest substring without repeating characters.
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = longest = 0 
+        visited = {}
+        
+        for i in range(len(s)):
+            if s[i] in visited and start <= visited[s[i]]:
+                start = visited[s[i]] + 1 
+            else:
+                longest = max(longest, i - start + 1)
+            
+            visited[s[i]] = i
+        
+        return longest
