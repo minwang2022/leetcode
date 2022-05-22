3e01fd78baa0beb93b09bd160fe38608e13ad181
min <minwang.bus@gmail.com>
2022-05-22 14:53:27 -0400

    top questions 7

--- top_questions.py
+++ top_questions.py
@@ -399,3 +399,34 @@ class Solution:
         else:
             answer += time[4]
         return answer
+
+# 407 · Plus One
+# Description
+# Given a non-negative number represented as an array of digits, plus one to the number.Returns a new array.
+
+# The number is arranged according to the number of digits, with the highest digit at the top of the list.
+
+# Example
+# Example 1:
+
+# Input: [1,2,3]
+# Output: [1,2,4]
+# Example 2:
+
+# Input: [9,9,9]
+# Output: [1,0,0,0]
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param digits: a number represented as an array of digits
+    @return: the result
+    """
+    def plus_one(self, digits: List[int]) -> List[int]:
+        # write your code here
+        num = int("".join([str(digit) for digit in digits]))
+        num += 1 
+        return [int(ele) for ele in str(num)]
