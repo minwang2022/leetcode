6c8cb6e1ae3ff26cfcde2eb4c0b5c33bedeccf48
min <minwang.bus@gmail.com>
2022-05-22 21:01:22 -0400

    top questions 1

--- top_questions.py
+++ top_questions.py
@@ -743,3 +743,48 @@ class Solution:
                     seen.add(next_status)
         
         return -1
+
+# 53 · Reverse Words in a String
+# Description
+# Given an input string, reverse the string word by word.
+
+# What constitutes a word?
+# A sequence of non-space characters constitutes a word and some words have punctuation at the end.
+# Could the input string contain leading or trailing spaces?
+# Yes. However, your reversed string should not contain leading or trailing spaces.
+# How about multiple spaces between two words?
+# Reduce them to a single space in the reversed string.
+# Example
+# Example 1:
+
+# Input:
+
+# s = "the sky is blue"
+# Output:
+
+# "blue is sky the"
+# Explanation:
+
+# return a reverse the string word by word.
+# Example 2:
+
+# Input:
+
+# s = "hello world"
+# Output:
+
+# "world hello"
+
+class Solution:
+    """
+    @param s: A string
+    @return: A string
+    """
+    def reverse_words(self, s: str) -> str:
+        # write your code here
+        if not s:
+            return ""
+        s = [word for word in s.split()]
+        s.reverse()
+
+        return " ".join(s)
