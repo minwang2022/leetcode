d47567e0cdbbc781a43042427ad16d815acc0fd8
min <minwang.bus@gmail.com>
2022-05-31 16:07:31 -0400

    meeting room2

--- mock_interview.py
+++ mock_interview.py
@@ -649,3 +649,72 @@ class Solution:
         targets = counter.most_common(k)
         res = [num for num, target in targets]
         return res 
+
+
+# 919 · Meeting Rooms II
+# Description
+# Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.)
+
+# Contact me on wechat to get Amazon、Google requent Interview questions . (wechat id : jiuzhang15)
+
+
+# (0,8),(8,10) is not conflict at 8
+
+# Example
+# Example1
+
+# Input: intervals = [(0,30),(5,10),(15,20)]
+# Output: 2
+# Explanation:
+# We need two meeting rooms
+# room1: (0,30)
+# room2: (5,10),(15,20)
+# Example2
+
+# Input: intervals = [(2,7)]
+# Output: 1
+# Explanation: 
+# Only need one meeting room
+
+import collections 
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+    def min_meeting_rooms(self, intervals: List[Interval]) -> int:
+        # Write your code here
+        # visited = collections.defaultdict(int)
+        # count = 0
+
+        # for interval in intervals:
+        #     start, end = interval.start, interval.end
+        #     while start < end:
+                
+        #         visited[start] = visited.get(start, 0) +1 
+                
+        #         count = max(count, visited[start])
+                    
+        #         start += 1
+        # # print(visited)
+        # return count
+        points = []
+        for interval in intervals:
+            points.append((interval.start, 1))
+            points.append((interval.end, -1))
+        print(sorted(points))
+        meeting_rooms = 0
+        ongoing_meetings = 0
+        for _, delta in sorted(points):
+            ongoing_meetings += delta
+            meeting_rooms = max(meeting_rooms, ongoing_meetings)
+            
+        return meeting_rooms
