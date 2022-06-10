7855cca7147a4ddf53d124d6f608a0121833730e
min <minwang.bus@gmail.com>
2022-06-10 11:19:23 -0400

    word ladder

--- mock_interview.py
+++ mock_interview.py
@@ -842,3 +842,60 @@ class Solution:
                 dp[i] = max(dp[i], dp[start] + value)
         
         return max(dp)
+
+
+# 127. Word Ladder
+# A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+# Every adjacent pair of words differs by a single letter.
+# Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+# sk == endWord
+# Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+ 
+
+# Example 1:
+
+# Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+# Output: 5
+# Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+# Example 2:
+
+# Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+# Output: 0
+# Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+class Solution:
+    DICTIONARY = ["a", "b", "c","d", "e", "f","g", "h", "i","j", "k", "l","m", "n", "o","p", "q", "r","s", "t", "u","v", "w", "x","y", "z"]
+    import collections
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if not beginWord or not endWord:
+            return 0 
+        
+        que = collections.deque([beginWord])
+        visited = {beginWord: 1}
+        new_dict = {}
+        for word in wordList:
+            new_dict[word] = 1
+        while que:
+            word = que.popleft()
+            if word == endWord:
+                return visited[word]
+            
+            if self.generate_words(new_dict, visited, que, word):
+                self.generate_words(new_dict, visited, que, word)
+            
+        return 0 
+    
+    def generate_words(self, wordList, visited, que, word):
+        DICTIONARY = ["a", "b", "c","d", "e", "f","g", "h", "i","j", "k", "l","m", "n", "o","p", "q", "r","s", "t", "u","v", "w", "x","y", "z"]
+        for i in range(len(word)):
+            for ele in DICTIONARY:
+                if word[i] == ele:
+                    continue 
+                
+                new_word = word[:i] + ele + word[i + 1:]
+                if not new_word in visited and new_word in wordList:
+                    visited[new_word] = visited[word] + 1
+                    que.append(new_word)
+                
+        return True 
