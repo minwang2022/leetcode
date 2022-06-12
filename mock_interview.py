44fbf7350f602119fd97568870f1bd08d24fcb27
min <minwang.bus@gmail.com>
2022-06-12 14:38:04 -0400

    stock profit 1

--- mock_interview.py
+++ mock_interview.py
@@ -1001,3 +1001,45 @@ class Solution:
             visited = visited.union(local_set)
         return res
 
+# 149 · Best Time to Buy and Sell Stock
+# Description
+# Say you have an array for which the ith element is the price of a given stock on day i.
+
+# If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+# Contact me on wechat to get Amazon、Google requent Interview questions . (wechat id : jiuzhang0607)
+
+
+# Example
+# Example 1
+
+# Input: [3, 2, 3, 1, 2]
+# Output: 1
+# Explanation: You can buy at the third day and then sell it at the 4th day. The profit is 2 - 1 = 1
+# Example 2
+
+# Input: [1, 2, 3, 4, 5]
+# Output: 4
+# Explanation: You can buy at the 0th day and then sell it at the 4th day. The profit is 5 - 1 = 4
+# Example 3
+
+# Input: [5, 4, 3, 2, 1]
+# Output: 0
+# Explanation: You can do nothing and get nothing.
+
+class Solution:
+    """
+    @param prices: Given an integer array
+    @return: Maximum profit
+    """
+    def max_profit(self, prices: List[int]) -> int:
+        # write your code here
+        maxProfit, low = 0, sys.maxsize
+        for num in prices:
+            if num -low > maxProfit:
+                maxProfit = num - low
+            
+            if num < low:
+                low = num 
+        
+        return maxProfit
