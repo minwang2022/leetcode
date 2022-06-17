a3d47bd489dcb2dc397e5110f8f268e53ea826ce
min <minwang.bus@gmail.com>
2022-06-17 14:50:23 -0400

    google1

--- google.py
+++ google.py
@@ -42,4 +42,29 @@ class Solution:
             if len(left) == len(right):
                 res += 1
         
-        return res 
+        return res
+        
+# 1509. Minimum Difference Between Largest and Smallest Value in Three Moves
+# You are given an integer array nums. In one move, you can choose one element of nums and change it by any value.
+
+# Return the minimum difference between the largest and smallest value of nums after performing at most three moves.
+# Example 1:
+
+# Input: nums = [5,3,2,4]
+# Output: 0
+# Explanation: Change the array [5,3,2,4] to [2,2,2,2].
+# The difference between the maximum and minimum is 2-2 = 0.
+# Example 2:
+
+# Input: nums = [1,5,0,10,14]
+# Output: 1
+# Explanation: Change the array [1,5,0,10,14] to [1,1,0,1,1]. 
+# The difference between the maximum and minimum is 1-0 = 1.
+
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        if len(nums) < 4:
+            return 0
+        nums.sort()
+        
+        return min(nums[-1] - nums[3], nums[-2] - nums[2], nums[-3] - nums[1], nums[-4] - nums[0])
