a13731aa1c7f9b8e82c1c1e8bee81719617bfcf6
min <minwang.bus@gmail.com>
2022-06-17 14:49:28 -0400

    google

---
+++ google.py
@@ -0,0 +1,45 @@
+# 1525. Number of Good Ways to Split a String
+# You are given a string s.
+
+# A split is called good if you can split s into two non-empty strings sleft and sright where their concatenation is equal to s (i.e., sleft + sright = s) and the number of distinct letters in sleft and sright is the same.
+
+# Return the number of good splits you can make in s.
+# Example 1:
+
+# Input: s = "aacaba"
+# Output: 2
+# Explanation: There are 5 ways to split "aacaba" and 2 of them are good. 
+# ("a", "acaba") Left string and right string contains 1 and 3 different letters respectively.
+# ("aa", "caba") Left string and right string contains 1 and 3 different letters respectively.
+# ("aac", "aba") Left string and right string contains 2 and 2 different letters respectively (good split).
+# ("aaca", "ba") Left string and right string contains 2 and 2 different letters respectively (good split).
+# ("aacab", "a") Left string and right string contains 3 and 1 different letters respectively.
+# Example 2:
+
+# Input: s = "abcd"
+# Output: 1
+# Explanation: Split the string as follows ("ab", "cd").
+class Solution:
+    def numSplits(self, s: str) -> int:
+        if len(s) <= 1:
+            return 0
+        if len(s) == 2:
+            return 1
+        
+        left, right, res = {}, {}, 0
+        
+        for char in s:
+            right[char] = right.get(char, 0) + 1
+        
+        for char in s:
+            left[char] = left.get(char, 0) + 1
+            if right[char] == 1:
+                del right[char]
+            
+            else:
+                right[char] -= 1
+            
+            if len(left) == len(right):
+                res += 1
+        
+        return res 
