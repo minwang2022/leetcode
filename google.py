a483d957719d56e7792c0ecf862f2f0cb2959b99
min <minwang.bus@gmail.com>
2022-06-18 09:56:02 -0400

    google2

--- google.py
+++ google.py
@@ -68,3 +68,33 @@ class Solution:
         nums.sort()
         
         return min(nums[-1] - nums[3], nums[-2] - nums[2], nums[-3] - nums[1], nums[-4] - nums[0])
+
+
+# 949. Largest Time for Given Digits
+# Given an array arr of 4 digits, find the latest 24-hour time that can be made using each digit exactly once.
+
+# 24-hour times are formatted as "HH:MM", where HH is between 00 and 23, and MM is between 00 and 59. The earliest 24-hour time is 00:00, and the latest is 23:59.
+
+# Return the latest 24-hour time in "HH:MM" format. If no valid time can be made, return an empty string.
+# Example 1:
+
+# Input: arr = [1,2,3,4]
+# Output: "23:41"
+# Explanation: The valid 24-hour times are "12:34", "12:43", "13:24", "13:42", "14:23", "14:32", "21:34", "21:43", "23:14", and "23:41". Of these times, "23:41" is the latest.
+# Example 2:
+
+# Input: arr = [5,5,5,5]
+# Output: ""
+# Explanation: There are no valid 24-hour times as "55:55" is not valid.
+
+from itertools import permutations
+
+class Solution:
+    def largestTimeFromDigits(self, arr: List[int]) -> str:
+        newArr = list(permutations(sorted(arr, reverse=True)))
+        
+        for h1, h2, m1, m2 in newArr:
+            if h1 * 10 + h2 < 24 and m1 * 10 + m2 < 60:
+                return f'{h1}{h2}:{m1}{m2}'
+        return ''
+    
