6471cdfd7615c1e43e1b07db07347a8d80544c9f
min <minwang.bus@gmail.com>
2022-06-26 13:18:42 -0400

    google dfs multi level combination sum

--- dfs.py
+++ dfs.py
@@ -809,3 +809,40 @@ class Solution:
         
         dfs(newCandidates, target, 0, [])
         return combinations
+
+# 153 · Combination Sum II
+# Description
+# Given an array num and a number target. Find all unique combinations in num where the numbers sum to target.
+class Solution:
+    """
+    @param num: Given the candidate numbers
+    @param target: Given the target number
+    @return: All the combinations that sum to target
+             we will sort your return value in output
+    """
+    def combinationSum2(self, num, target):
+        # write your code here
+        nums = sorted(num)
+        subset,result = [],[]
+        start_index = 0
+        self.dfs(nums, target, start_index,subset,result)
+        return result
+        
+    def dfs(self, nums, target, start_index,subset,result):
+        # combination sum 例题的出口
+        if target ==0:
+            result.append(subset[:])
+            return 
+        
+        for i in range(start_index, len(nums)):
+
+            if nums[i]>target:
+                return 
+            # print("subset",subset)
+            # subset with duplicate 的判定
+            if nums[i] == nums[i-1] and i > start_index:
+                continue 
+            subset.append(nums[i])
+
+            self.dfs(nums, target-nums[i], i+1,subset,result)
+            subset.pop()
